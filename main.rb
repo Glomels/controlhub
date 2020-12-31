c5892510bd88761fab315d18d12f4321a6dcd472
Luis Lomeli <luis.lomeli@bfgo.mx>
2020-12-31 15:04:07 -0800

    add index parameter to endpoint route

--- main.rb
+++ main.rb
@@ -1,11 +1,11 @@
 require "sinatra"
 
-get '/api/fibindex' do
-    getFibonacciValueFromIndex(3).to_s
+get '/api/fibindex/:index' do
+    getFibonacciValueFromIndex(params['index'].to_i)
 end
 
  
 def getFibonacciValueFromIndex(value)
     index = (1.6180339**value - (-0.6180339)**value) / 2.236067977
-    return index.round()
+    return index.round().to_s
 end

b0f84142333fc7c2a80164292e96747c4bfd6240
Luis Lomeli <luis.lomeli@bfgo.mx>
2020-12-31 15:27:05 -0800

    add simple cache to avoid calculation for known values

--- main.rb
+++ main.rb
@@ -1,11 +1,27 @@
 require "sinatra"
+require "sinatra/json"
+
+$cache = {}
 
 get '/api/fibindex/:index' do
-    getFibonacciValueFromIndex(params['index'].to_i)
+    cached = checkCache(params['index'])
+
+    if cached != nil then
+        puts "got cached value"
+        return json :value => cached
+    end
+
+    return json :value => getFibonacciValueFromIndex(params['index'].to_i)
 end
 
  
 def getFibonacciValueFromIndex(index)
-    value = (1.6180339**index - (-0.6180339)**index) / 2.236067977
-    return value.round().to_s
+    value = ((1.6180339**index - (-0.6180339)**index) / 2.236067977).round()
+    $cache[index.to_s] = value
+    return value
+end
+
+def checkCache(index)
+    puts index.to_s
+    return $cache[index]
 end
